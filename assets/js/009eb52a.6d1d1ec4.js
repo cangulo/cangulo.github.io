1593914f88d5b4fb28ef143b5d412ecac462b320
cangulo_gh_action <cangulo_gh_action@github.com>
2021-11-07 21:54:41 +0000

    fix: updated gh page

---
+++ assets/js/009eb52a.6d1d1ec4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcangulo_github_io=self.webpackChunkcangulo_github_io||[]).push([[1640],{3232:function(e){e.exports=JSON.parse('{"permalink":"/projects","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
