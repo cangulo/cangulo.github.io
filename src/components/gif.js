f6f544fc190081273971bc29e38a91008df91ab0
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-12-08 14:23:20 +0100

    update gif component

--- src/components/gif.js
+++ src/components/gif.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Gif = ({ link }) => {
     return (<div className="gif-iframe">
         {/* <iframe src={link} width="480" height="480" frameBorder="0" className="giphy-embed" /> */}
-        <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src={link} width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/success-working-admin-KAqM1EwUVeTGWbNwfZ">via GIPHY</a></p>
+        <div style="width:100%;height:0;padding-bottom:100%;position:relative;">
+            <iframe src={link} width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        </div>
+        <p>
+            <a href={link}>via GIPHY</a>
+        </p>
     </div >)
 };
 
