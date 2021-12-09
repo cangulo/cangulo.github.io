71f4a93a3015df9b5d5f868683342f132d4672bf
cangulo_gh_action <cangulo_gh_action@github.com>
2021-12-09 11:28:43 +0000

    fix: updated gh page

--- assets/js/53557ab6.7d8cb5e1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcangulo_github_io=self.webpackChunkcangulo_github_io||[]).push([[2293],{6756:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"What is documentation ?","permalink":"/values/documentation"},{"title":"some points about being developer","permalink":"/values/developer"}]}')}}]);
