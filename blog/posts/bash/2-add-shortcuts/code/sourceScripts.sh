3c3b46513722288e7a1619d967ab5ce36fbcf768
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-12-13 11:18:03 +0100

    fix

--- blog/posts/bash/2-add-shortcuts/code/sourceScripts.sh
+++ blog/posts/bash/2-add-shortcuts/code/sourceScripts.sh
@@ -1,5 +1,5 @@
 # update this to your scripts folder
 scriptsFolder="YOUR_PATH/scripts"
 
-# source all scripts (`.` and `source` are the same commands)
+# source all scripts (instead of `source`, we could use `.` both are the same command)
 for i in $scriptsFolder/*.sh; do source "$i"; done
