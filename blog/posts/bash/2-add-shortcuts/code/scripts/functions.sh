9acbe8d548adf10295481cb85efd4018e53a1618
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-12-13 22:56:55 +0100

    cleancode

--- blog/posts/bash/2-add-shortcuts/code/scripts/functions.sh
+++ blog/posts/bash/2-add-shortcuts/code/scripts/functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 openRepoUrl() {
-    gitUrl=$(git remote get-url --all origin)
-    repoUrl="${gitUrl%".git"}" # Removing .git prefix
+    local gitUrl=$(git remote get-url --all origin)
+    local repoUrl="${gitUrl%".git"}" # Removing .git prefix
     if [[ -n "$repoUrl" ]]; then
         echo "repoUrl:$repoUrl"
         xdg-open $repoUrl
