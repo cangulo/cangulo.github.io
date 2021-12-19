b2cffabc696962f68e2608d95328ded5a7a9aeb5
cangulo_gh_action <cangulo_gh_action@github.com>
2021-12-19 19:12:18 +0000

    fix: updated gh page

--- assets/js/357e6a7b.db391161.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcangulo_github_io=self.webpackChunkcangulo_github_io||[]).push([[4050],{1565:function(e){e.exports=JSON.parse('{"title":"Cheatsheets","items":[{"title":"Git Cheatsheet","permalink":"/cheatsheets/git"},{"title":"Terraform Cheatsheet","permalink":"/cheatsheets/terraform"}]}')}}]);
