d7cb5403075407341745aecc4a34549aa938f2d1
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-12-19 19:33:42 +0100

    fix error that show comment in post list

--- src/remark/add-comment-area.js
+++ src/remark/add-comment-area.js
@@ -1,14 +1,17 @@
 const plugin = (options) => {
     const transformer = async (ast) => {
 
-        ast.children.push({
-            type: 'import',
-            value: "import Comments from '/src/components/blog/comments.js'\n"
-        })
-        ast.children.push({
-            type: 'jsx',
-            value: '<Comments />'
-        })
+        let isFullBody = ast.children.filter(x => x.type == 'comment' && x.value.includes('truncate')).length > 0
+        if (isFullBody) {
+            ast.children.push({
+                type: 'import',
+                value: "import Comments from '/src/components/blog/comments.js'\n"
+            })
+            ast.children.push({
+                type: 'jsx',
+                value: '<Comments />'
+            })
+        }
     };
     return transformer;
 };
